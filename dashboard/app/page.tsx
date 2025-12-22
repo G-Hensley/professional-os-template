e034bd846d34bedb70467c65c68ab428c5fadde3
Gavin Hensley <gavinhensley@protonmail.com>
2025-12-22 18:49:57 -0500

    Add basic sidebar

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -2,9 +2,9 @@ import { Footer, TopBar, SideBar } from "@components/layout";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen h-fit">
+    <main className="flex flex-col h-screen min-h-fit px-2 relative">
       <TopBar />
-      <section>
+      <section className="h-full flex items-center">
         <SideBar />
       </section>
       <Footer />
--- dashboard/src/components/layout/SideBar.tsx
+++ dashboard/src/components/layout/SideBar.tsx
@@ -1,7 +1,26 @@
+import Link from "next/dist/client/link";
+
 function SideBar() {
+  const links = [
+    { href: "/", label: "Home" },
+    { href: "/about", label: "About" },
+    { href: "/contact", label: "Contact" },
+  ]
+
   return (
-    <nav aria-label="Sidebar" className="bg-teal-950">
+    <nav aria-label="Sidebar" className="neumorphic rounded-4xl w-fit relative h-fit">
       {/* SideBar content goes here */}
+      <div className="flex flex-col">
+        {links.map(({ href, label }) => (
+          <Link 
+            key={href} 
+            href={href} 
+            className="p-3 text-orange-400 text-center first:rounded-t-4xl last:rounded-b-4xl hover:bg-teal-900 hover:text-orange-500 transition-colors duration-300 ease-in-out"
+          >
+            {label}
+          </Link>
+        ))}
+      </div>
     </nav>
   );
 }
