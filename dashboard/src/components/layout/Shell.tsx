254c69f35ded510c6b23491acfb5a6900625b1ce
Gavin Hensley <gavinhensley@protonmail.com>
2025-12-24 22:49:42 -0500

    Adjust the height and spacing of the overlay

--- dashboard/src/components/layout/Shell.tsx
+++ dashboard/src/components/layout/Shell.tsx
@@ -6,16 +6,16 @@ interface ShellProps {
 
 function Shell({ children }: ShellProps) {
   return (
-    <section className="neumorphic w-fit xl:max-h-[80vh] rounded-4xl
+    <section className="neumorphic w-fit xl:max-h-[75vh] rounded-4xl
       overflow-y-scroll scrollbar-thin scrollbar-thumb-cyan-800 scrollbar-track-cyan-900
       relative"
     >
-      <div className="px-8 py-6 flex flex-wrap gap-6 justify-center">
+      <div className="px-8 py-4 flex flex-wrap gap-6 justify-center">
         {children}
       </div>
       <div
-        className="sticky bottom-0 left-0 right-0 w-full h-36 z-10
-          bg-linear-to-t from-black/50 to-transparent
+        className="sticky bottom-0 left-0 right-0 w-full h-24 -mt-24 z-10
+          bg-linear-to-t from-black/40 to-transparent
           pointer-events-none rounded-b-4xl"
       />
     </section>
